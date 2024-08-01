3e8a935f567d9324af96505d0895b9970380c9d1
LargePenguin034 <134495820+LargePenguin034@users.noreply.github.com>
2024-08-02 09:02:29 +1000

    file structure set up part 2

---
+++ src/services/api.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../components/specific/Header';
+
+const Home = () => (
+  <div>
+    <Header />
+    <h2>Welcome to the Home Page</h2>
+  </div>
+);
+
+export default Home;
